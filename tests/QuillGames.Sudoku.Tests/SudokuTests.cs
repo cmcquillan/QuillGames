52fec3e7aaa03ab23d359f162bbd4a979e0fe8c2
Casey McQuillan <Casey@quilltechnologies.com>
2020-06-27 16:56:56 -0700

    Initial Commit

---
+++ tests/QuillGames.Sudoku.Tests/SudokuTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace QuillGames.Sudoku.Tests
+{
+    public class SudokuTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

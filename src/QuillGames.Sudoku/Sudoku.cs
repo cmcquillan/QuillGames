e44a0bb3109542011117b9548bc752ec434b035c
Casey McQuillan <Casey@quilltechnologies.com>
2020-06-28 11:03:14 -0700

    Completed a native sudoku checker.

--- src/QuillGames.Sudoku/Sudoku.cs
+++ src/QuillGames.Sudoku/Sudoku.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace QuillGames.Sudoku
@@ -9,17 +10,19 @@ namespace QuillGames.Sudoku
         public const int Unfilled = 0;
 
         /*
-         * +--------------------------+
-         * | 0| 1| 2| 3| 4| 5| 6| 7| 8|
-         * | 9|10|11|12|13|14|15|16|17|
-         * |18|19|20|21|22|23|24|25|26|
-         * |27|28|29|30|31|32|33|34|35|
-         * |36|37|38|39|40|41|42|43|44|
-         * |45|46|47|48|49|50|51|52|52|
-         * |54|55|56|57|58|59|60|61|62|
-         * |63|64|65|66|67|68|69|70|71|
-         * |72|73|74|75|76|77|78|79|80|
-         * +--------------------------+
+         *      Cols
+         *        1  2  3  4  5  6  7  8  9
+         * Rows +--------------------------+
+         *   1  | 0| 1| 2| 3| 4| 5| 6| 7| 8|
+         *   2  | 9|10|11|12|13|14|15|16|17|
+         *   3  |18|19|20|21|22|23|24|25|26|
+         *   4  |27|28|29|30|31|32|33|34|35|
+         *   5  |36|37|38|39|40|41|42|43|44|
+         *   6  |45|46|47|48|49|50|51|52|52|
+         *   7  |54|55|56|57|58|59|60|61|62|
+         *   8  |63|64|65|66|67|68|69|70|71|
+         *   9  |72|73|74|75|76|77|78|79|80|
+         *      +--------------------------+
          * 
          * Represents the numbers in the puzzle.
          * We will use 0 to denote an unfilled spot.
@@ -46,7 +49,83 @@ namespace QuillGames.Sudoku
 
         public bool Check()
         {
-            return false;
+            bool IsValid(int[] numbers) => numbers.Distinct().Count() == 9;
+
+            // Assume valid until we see a bad row, col, or square.
+            bool valid = true;
+            int ix = 0;
+            do
+            {
+                // Row
+                valid = valid && IsValid(GetRow(ix + 1));
+
+                // Col
+                valid = valid && IsValid(GetCol(ix + 1));
+
+                // Square
+                valid = valid && IsValid(GetSquare(ix + 1));
+
+                ix++;
+            } while (valid && ix < 9);
+
+            return valid;
+        }
+
+        /// <summary>
+        /// Gets a single cell from the puzzle. Rows and columns are indexed by 1.
+        /// </summary>
+        /// <remarks>
+        /// Every access to a number should go through this method. If we follow
+        /// this rule, then changing the way that the puzzle is 'backed' (such as 
+        /// from a single array to a multi-dimensional), would only require changing
+        /// this method.
+        /// </remarks>
+        /// <returns>The number at the cell.</returns>
+        public int GetCell(int row, int col)
+        {
+            int r = row - 1, c = col - 1;
+            return _puzzle[(r * 9) + c];
+        }
+
+        public int[] GetRow(int row)
+        {
+            int[] rowNumbers = new int[9];
+            for (int i = 0; i < rowNumbers.Length; i++)
+            {
+                rowNumbers[i] = GetCell(row, i + 1);
+            }
+
+            return rowNumbers;
+        }
+
+        public int[] GetCol(int col)
+        {
+            int[] colNumbers = new int[9];
+            for (int i = 0; i < colNumbers.Length; i++)
+            {
+                colNumbers[i] = GetCell(i + 1, col);
+            }
+
+            return colNumbers;
+        }
+
+        public int[] GetSquare(int square)
+        {
+            int[] squareNumbers = new int[9];
+            int rowOffset = ((square - 1) % 3 * 3) + 1;
+            int colOffset = ((square - 1) % 3 * 3) + 1;
+
+            int ix = 0;
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    squareNumbers[ix] = GetCell(row + rowOffset, col + colOffset);
+                    ix++;
+                }
+            }
+
+            return squareNumbers;
         }
     }
 }
--- tests/QuillGames.Sudoku.Tests/SudokuTests.cs
+++ tests/QuillGames.Sudoku.Tests/SudokuTests.cs
@@ -6,7 +6,7 @@ namespace QuillGames.Sudoku.Tests
     public class SudokuTests
     {
         [Fact]
-        public void Sudoku_ValidPuzzlesShouldCheckAsTrue()
+        public void Sudoku_Check_ValidPuzzlesShouldCheckAsTrue()
         {
             // Arrange
             var puzzle = new Sudoku(ValidPuzzles.Puzzle1);
@@ -16,6 +16,124 @@ namespace QuillGames.Sudoku.Tests
 
             // Assert
             Assert.True(result);
+        }
+
+        [Fact]
+        public void Sudoku_GetCell_ReturnsNumberAtLocation()
+        {
+            // Arrange
+            var puzzle = new Sudoku(ValidPuzzles.Puzzle1);
+
+            // Act
+            var num = puzzle.GetCell(1, 1);
+
+            // Assert
+            Assert.Equal(4, num);
+        }
+
+        [Fact]
+        public void Sudoku_GetRow_ReturnsNumbersFromRow()
+        {
+            // Arrange
+            var puzzle = new Sudoku(ValidPuzzles.Puzzle1);
+
+            // Act
+            var row = puzzle.GetRow(1);
+
+            // Assert
+            Assert.Equal(4, row[0]);
+            Assert.Equal(3, row[1]);
+            Assert.Equal(5, row[2]);
+            Assert.Equal(2, row[3]);
+            Assert.Equal(6, row[4]);
+            Assert.Equal(9, row[5]);
+            Assert.Equal(7, row[6]);
+            Assert.Equal(8, row[7]);
+            Assert.Equal(1, row[8]);
+        }
+
+        [Fact]
+        public void Sudoku_GetCol_ReturnsNumbersFromColumn()
+        {
+            // Arrange
+            var puzzle = new Sudoku(ValidPuzzles.Puzzle1);
+
+            // Act
+            var col = puzzle.GetCol(1);
+
+            // Assert
+            Assert.Equal(4, col[0]);
+            Assert.Equal(6, col[1]);
+            Assert.Equal(1, col[2]);
+            Assert.Equal(8, col[3]);
+            Assert.Equal(3, col[4]);
+            Assert.Equal(9, col[5]);
+            Assert.Equal(5, col[6]);
+            Assert.Equal(2, col[7]);
+            Assert.Equal(7, col[8]);
+        }
+
+        [Fact]
+        public void Sudoku_GetCol_ReturnsNumbersFromMiddleColumn()
+        {
+            // Arrange
+            var puzzle = new Sudoku(ValidPuzzles.Puzzle1);
+
+            // Act
+            var col = puzzle.GetCol(2);
+
+            // Assert
+            Assert.Equal(3, col[0]);
+            Assert.Equal(8, col[1]);
+            Assert.Equal(9, col[2]);
+            Assert.Equal(2, col[3]);
+            Assert.Equal(7, col[4]);
+            Assert.Equal(5, col[5]);
+            Assert.Equal(1, col[6]);
+            Assert.Equal(4, col[7]);
+            Assert.Equal(6, col[8]);
+        }
+
+        [Fact]
+        public void Sudoku_GetSquare_ReturnsNumbersFromSquare()
+        {
+            // Arrange
+            var puzzle = new Sudoku(ValidPuzzles.Puzzle1);
+
+            // Act
+            var square = puzzle.GetSquare(1);
+
+            // Assert
+            Assert.Equal(4, square[0]);
+            Assert.Equal(3, square[1]);
+            Assert.Equal(5, square[2]);
+            Assert.Equal(6, square[3]);
+            Assert.Equal(8, square[4]);
+            Assert.Equal(2, square[5]);
+            Assert.Equal(1, square[6]);
+            Assert.Equal(9, square[7]);
+            Assert.Equal(7, square[8]);
+        }
+
+        [Fact]
+        public void Sudoku_GetSquare_ReturnsNumbersFromMiddleSquare()
+        {
+            // Arrange
+            var puzzle = new Sudoku(ValidPuzzles.Puzzle1);
+
+            // Act
+            var square = puzzle.GetSquare(5);
+
+            // Assert
+            Assert.Equal(1, square[0]);
+            Assert.Equal(9, square[1]);
+            Assert.Equal(5, square[2]);
+            Assert.Equal(6, square[3]);
+            Assert.Equal(8, square[4]);
+            Assert.Equal(2, square[5]);
+            Assert.Equal(7, square[6]);
+            Assert.Equal(4, square[7]);
+            Assert.Equal(3, square[8]);
         }
     }
 }
